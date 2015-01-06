6edc3fc30595efead9e1e068d04ef7fdb50ddcb5
Zyluss <Zyluss@users.noreply.github.com>
2015-01-06 19:22:28 +0000

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	var d = new Date();
 	var n = d.getFullYear();
 	document.getElementById("currentDate").innerHTML = n;
+//LINKAGE
+
+$("#copyright").click(function(){
+  window.location = "/license";
+   });
 });
